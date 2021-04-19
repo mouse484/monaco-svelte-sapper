8de5d04e20264739c074424940ee6b4ae22bd020
mouse_484 <escmouse484@gmail.com>
2021-04-19 07:54:17 +0000

    monaco first

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,50 +1,23 @@
-<script>
-	import successkid from 'images/successkid.jpg';
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let container;
+  let editor;
+
+  onMount(() => {
+    import('monaco-editor').then((monaco) => {
+      editor = monaco.editor.create(container, {
+        language: 'typescript',
+      });
+    });
+  });
 </script>
 
+<div bind:this={container} id="monaco-editor" />
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  #monaco-editor {
+    height: 80vh;
+    width: 100%;
+  }
 </style>
-
-<svelte:head>
-	<title>Sapper project template</title>
-</svelte:head>
-
-<h1>Great success!</h1>
-
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
